2907d225ac1a2326423890ab65befa541ea7f9f3
wgmv <wvogel@generation2.de>
2017-09-20 00:27:27 +0200

    code cleanup

--- src/Methods/SlackMethod.php
+++ src/Methods/SlackMethod.php
@@ -3,7 +3,6 @@
 namespace Wgmv\SlackApi\Methods;
 
 use Wgmv\SlackApi\Contracts\SlackApi;
-use Illuminate\Contracts\Cache\Repository as Cache;
 
 abstract class SlackMethod
 {
@@ -17,22 +16,13 @@ abstract class SlackMethod
      * @var \Wgmv\SlackApi\Contracts\SlackApi
      */
     protected $api;
-//
-//    /**
-//     * @var Cache
-//     */
-//    protected $cache;
-
-//    protected $cachePrefix = '__vlz_slackc_';
 
     /**
      * @param \Wgmv\SlackApi\Contracts\SlackApi  $api
-     * @param Cache $cache
      */
     public function __construct(SlackApi $api)
     {
         $this->api = $api;
-//        $this->cache = $cache;
     }
 
     /**
@@ -58,87 +48,4 @@ abstract class SlackMethod
         return $this->api;
     }
 
-//    /**
-//     * Cache a value.
-//     *
-//     * @param string $key
-//     * @param mixed  $value
-//     * @param int    $minutes Default 1
-//     *
-//     * @return mixed
-//     */
-//    public function cachePut($key, $value, $minutes = 1)
-//    {
-//        $this->cache->put($this->cachePrefix($key), $value, $minutes);
-//
-//        return $value;
-//    }
-//
-//    /**
-//     * Remember the result value for a given closure.
-//     * @param $key
-//     * @param $minutes
-//     * @param $callback
-//     *
-//     * @return mixed
-//     */
-//    public function cacheRemember($key, $minutes, $callback)
-//    {
-//        return $this->cache->remember($this->cachePrefix($key), $minutes, $callback);
-//    }
-//
-//    /**
-//     * Remember the result value for a closure forever.
-//     * @param $key
-//     * @param $callback
-//     *
-//     * @return mixed
-//     */
-//    public function cacheRememberForever($key, $callback)
-//    {
-//        return $this->cache->rememberForever($this->cachePrefix($key), $callback);
-//    }
-//
-//    /**
-//     * Get a cache for a given key.
-//     * @param string $key
-//     * @param null $default
-//     *
-//     * @return mixed
-//     */
-//    public function cacheGet($key, $default = null)
-//    {
-//        return $this->cache->get($this->cachePrefix($key), $default);
-//    }
-//
-//    /**
-//     * Cache a value forever.
-//     * @param $key
-//     * @param $value
-//     */
-//    public function cacheForever($key, $value)
-//    {
-//        $this->cache->forever($this->cachePrefix($key), $value);
-//    }
-//
-//    /**
-//     * Forget a value for a given key.
-//     * @param $key
-//     */
-//    public function cacheForget($key)
-//    {
-//        $this->cache->forget($this->cachePrefix($key));
-//    }
-//
-//    /**
-//     * Get the default key prefix.
-//     *
-//     * @param string|null $key
-//     *
-//     * @return string
-//     */
-//    protected function cachePrefix($key = null)
-//    {
-//        return $this->cachePrefix.$key;
-//    }
 }
