ba29470782700236c7031084406a15cfa81494a3
jakebathman <jake.bathman@gmail.com>
2019-11-01 16:50:28 -0500

    Update for Laravel 6.x
    - Add `illuminate` versions that support 6.x
    - Change `phpunit` version constraint to support `>=6.0`
    - Replace deprecated `Str` helper functions with their support class method implementations

--- src/SlackApiServiceProvider.php
+++ src/SlackApiServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Wgmv\SlackApi;
 
+use Illuminate\Support\Str;
 use Illuminate\Support\ServiceProvider;
 
 class SlackApiServiceProvider extends ServiceProvider
@@ -38,7 +39,7 @@ class SlackApiServiceProvider extends ServiceProvider
     public function register()
     {
         /* Lumen autoload services configs */
-        if (str_contains($this->app->version(), 'Lumen')) {
+        if (Str::contains($this->app->version(), 'Lumen')) {
             $this->app->configure('services');
         }
 
@@ -52,7 +53,7 @@ class SlackApiServiceProvider extends ServiceProvider
 
             $contract = "Wgmv\SlackApi\Contracts\Slack".$method;
             $class = "Wgmv\SlackApi\Methods\\".$method;
-            $shortcut = "slack.".snake_case($method);
+            $shortcut = "slack.".Str::snake($method);
 
             $this->app->singleton($contract, function () use ($class) {
                 return new $class($this->app['slack.api']);
@@ -73,7 +74,7 @@ class SlackApiServiceProvider extends ServiceProvider
         $shortcuts[] = 'slack.api';
 
         foreach ($this->methods as $method) {
-            $shortcuts[] = $shortcut = "slack.".snake_case($method);
+            $shortcuts[] = $shortcut = "slack.".Str::snake($method);
         }
         return $shortcuts;
     }
